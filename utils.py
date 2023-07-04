1152491edd5417285832bcc0b1f3e553425b435a
maher <mahernadar@gmail.com>
2023-07-04 19:14:39 +0200

    corrected paths for embeddings

--- utils.py
+++ utils.py
@@ -1,9 +1,8 @@
-import torch
 import pandas as pd
     
 
-MOVIES_EMBEDDINGS = pd.read_csv("movie-recommender/data/movies_embeddings.csv")
+MOVIES_EMBEDDINGS = pd.read_csv("data/movies_embeddings.csv")
 
-AGES_EMBEDDINGS = pd.read_csv("movie-recommender/data/ages_embeddings.csv")
+AGES_EMBEDDINGS = pd.read_csv("data/ages_embeddings.csv")
 
-GENDERS_EMBEDDINGS = pd.read_csv("movie-recommender/data/genders_embeddings.csv")
+GENDERS_EMBEDDINGS = pd.read_csv("data/genders_embeddings.csv")
